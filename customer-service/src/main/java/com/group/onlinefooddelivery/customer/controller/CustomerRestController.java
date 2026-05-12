24033c70fbdce6a3560c73826b599b14083a46e7
Malsha <malshapramodi2001@gmail.com>
2026-05-12 22:20:12 +0530

    Add Customer REST controller

---
+++ customer-service/src/main/java/com/group/onlinefooddelivery/customer/controller/CustomerRestController.java
@@ -0,0 +1,47 @@
+package com.group.onlinefooddelivery.customer.controller;
+
+import com.group.onlinefooddelivery.customer.domain.Customer;
+import com.group.onlinefooddelivery.customer.service.CustomerService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerRestController {
+
+    private final CustomerService customerService;
+
+    public CustomerRestController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer createCustomer(@Valid @RequestBody Customer customer) {
+        return customerService.createCustomer(customer);
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/{id}")
+    public Customer getCustomerById(@PathVariable Long id) {
+        return customerService.getCustomerById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Customer updateCustomer(@PathVariable Long id, @Valid @RequestBody Customer customer) {
+        return customerService.updateCustomer(id, customer);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+    }
+}
